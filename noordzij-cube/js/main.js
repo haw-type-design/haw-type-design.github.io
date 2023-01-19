0347557e2dce6a8bc3a77455f7252e5d94cc3752
simonthi <github@simonthiefes.de>
2023-01-19 16:52:58 +0100

    Tweaking control layout

--- noordzij-cube/js/main.js
+++ noordzij-cube/js/main.js
@@ -1,4 +1,10 @@
 function togglePopover() {
     var popup = document.getElementById("infoPopover");
     popup.classList.toggle("visible");
+    var hider = document.getElementById("info");
+    if (hider.innerHTML === "Info") {
+        hider.innerHTML = "Hide Info";
+    } else {
+        hider.innerHTML = "Info";
+    }
 }
